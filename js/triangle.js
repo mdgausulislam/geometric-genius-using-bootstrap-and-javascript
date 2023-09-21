d69cb3745549d412b696f827083cb2f14572deb2
unknown <gausulislam5@gmail.com>
2023-09-21 22:15:34 +0600

    update triangle

--- js/triangle.js
+++ js/triangle.js
@@ -1,20 +1,28 @@
-document.getElementById('btn-triangle').addEventListener('click',function(){
+document.getElementById('btn-triangle').addEventListener('click', function () {
     console.log('clicked');
-    const triangleB=getElementsById('triangle-b');
+    const triangleB = getElementsById('triangle-b');
     console.log(triangleB);
-    const triangleH=getElementsById('triangle-h');
+    const triangleH = getElementsById('triangle-h');
     console.log(triangleH);
 
-    const triangleTextB=getElementsByIdText('triangle-text-b');
-    setTextElementsById('triangle-text-b',triangleB)
+    const triangleTextB = getElementsByIdText('triangle-text-b');
+    setTextElementsById('triangle-text-b', triangleB)
 
-    const triangleTextH=getElementsByIdText('triangle-text-h');
-    setTextElementsById('triangle-text-h',triangleH);
+    const triangleTextH = getElementsByIdText('triangle-text-h');
+    setTextElementsById('triangle-text-h', triangleH);
 
-    const triangleOutput=0.5*triangleB*triangleH;
+    const triangleOutput = 0.5 * triangleB * triangleH;
     console.log(triangleOutput);
 
-    const triangleO=getElementsByIdText('triangle-Output');
-    setTextElementsById('triangle-Output',triangleOutput);
-    
+    const triangleO = getElementsByIdText('triangle-Output');
+    setTextElementsById('triangle-Output', triangleOutput);
+})
+document.getElementById('meter-convert').addEventListener('click', function () {
+    const meterConvert = getElementsByIdText('triangle-Output');
+    const meterOutput = meterConvert / 100;
+    setTextElementsById('triangle-Output',meterOutput);
+
+    const miterTextConvert=document.getElementById('meter-text-convert');
+    miterTextConvert.innerHTML='m<sup>2</sup>';
+
 })
